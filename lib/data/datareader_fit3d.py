31da0ad74cf647f021d93fb20c9a464867b7cd05
Andrew Boessen <boessena@bc.edu>
2024-04-24 12:38:38 -0400

    get 3d joints and split clips

--- lib/data/datareader_fit3d.py
+++ lib/data/datareader_fit3d.py
@@ -62,3 +62,70 @@ class DataReaderFit3D(object):
             trainset = np.concatenate((trainset, train_confidence), axis=2)  # [N, 17, 3]
             testset = np.concatenate((testset, test_confidence), axis=2)  # [N, 17, 3]
         return trainset, testset
+
+    def read_3d(self):
+        """
+        Read 3D joint data from dataset
+
+        Returns train and test sets ([N, 17, 3])
+        """
+        train_labels = self.dt_dataset['train']['joint3d_image'][::self.sample_stride, :, :3].astype(np.float32)  # [N, 17, 3]
+        test_labels = self.dt_dataset['test']['joint3d_image'][::self.sample_stride, :, :3].astype(np.float32)    # [N, 17, 3]
+        # map to [-1, 1]
+        for idx, camera_name in enumerate(self.dt_dataset['train']['camera_name']):
+            if camera_name == '54138969' or camera_name == '60457274':
+                res_w, res_h = 1000, 1002
+            elif camera_name == '55011271' or camera_name == '58860488':
+                res_w, res_h = 1000, 1000
+            else:
+                assert 0, '%d data item has an invalid camera name' % idx
+            train_labels[idx, :, :2] = train_labels[idx, :, :2] / res_w * 2 - [1, res_h / res_w] # Normalize X and Y
+            train_labels[idx, :, 2:] = train_labels[idx, :, 2:] / res_w * 2 # Normalize 3rd dimension (Z)
+            
+        for idx, camera_name in enumerate(self.dt_dataset['test']['camera_name']):
+            if camera_name == '54138969' or camera_name == '60457274':
+                res_w, res_h = 1000, 1002
+            elif camera_name == '55011271' or camera_name == '58860488':
+                res_w, res_h = 1000, 1000
+            else:
+                assert 0, '%d data item has an invalid camera name' % idx
+            test_labels[idx, :, :2] = test_labels[idx, :, :2] / res_w * 2 - [1, res_h / res_w] # Normalize X and Y
+            test_labels[idx, :, 2:] = test_labels[idx, :, 2:] / res_w * 2 # Normalize 3rd dimension (Z)
+            
+        return train_labels, test_labels
+
+    def get_split_id(self):
+        """
+        Gets the split IDs based on frame labels for training and testing data.
+
+        Returns:
+            tuple: A tuple containing the split IDs for training and testing data,
+                or None for one or both if the corresponding video lists are missing.
+        """
+        if self.split_id_train is not None and self.split_id_test is not None:
+            # If split IDs for both training and testing data are already calculated, return them
+            return self.split_id_train, self.split_id_test
+
+        # Extract video lists from the dataset
+        vid_list_train = self.dt_dataset['train']['source'][::self.sample_stride]  # Labels for frames of train set (subject, action, camera). shpae: (1559752,)
+        vid_list_test = self.dt_dataset['test']['source'][::self.sample_stride]   # Labels for frames of test set (subject, action, camera). shpae: (566920,)
+
+        # Calculate split IDs for training and testing data
+        self.split_id_train = split_clips(vid_list_train, self.n_frames, data_stride=self.data_stride_train)
+        self.split_id_test = split_clips(vid_list_test, self.n_frames, data_stride=self.data_stride_test)
+
+        return self.split_id_train, self.split_id_test
+    
+    def get_sliced_data(self):
+        """
+        Gets the data and splits into traning and test sets
+
+        Returns: training and test data, label pairs (N, 27, 17, 3)
+        """
+        train_data, test_data = self.read_2d()     # train_data (1559752, 17, 3) test_data (566920, 17, 3)
+        train_labels, test_labels = self.read_3d() # train_labels (1559752, 17, 3) test_labels (566920, 17, 3)
+        split_id_train, split_id_test = self.get_split_id() # Split data into individual clips
+        train_data, test_data = train_data[split_id_train], test_data[split_id_test]                # (N, 27, 17, 3)
+        train_labels, test_labels = train_labels[split_id_train], test_labels[split_id_test]        # (N, 27, 17, 3)
+        # ipdb.set_trace()
+        return train_data, test_data, train_labels, test_labels
