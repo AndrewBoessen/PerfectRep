f29fb65feee9824c9fa8ce793521d2ab9d3b784e
Andrew Boessen <boessena@bc.edu>
2024-04-26 14:18:16 -0400

    process data set

--- compress_fit3d.py
+++ compress_fit3d.py
@@ -1,7 +1,9 @@
+import json
 import numpy as np
 import os, sys
 import pickle
 import torch
+import numpy as np
 
 from lib.utils.tools import ensure_dir
 
@@ -15,6 +17,8 @@ def project_to_2d(X, camera_params):
     Arguments:
     X -- 3D points in *camera space* to transform (N, *, 3)
     camera_params -- intrinsic parameteres (N, 2+2+3+2=9)
+
+    https://github.com/facebookresearch/VideoPose3D/blob/main/common/camera.py
     """
     assert X.shape[-1] == 3
     assert len(camera_params.shape) == 2
@@ -39,12 +43,104 @@ def project_to_2d(X, camera_params):
     
     return f*XXX + c
 
-def convert_data(data_path = 'data/fit3d_train/train', test_subjects = ['s03', 's011']):
+def split_data_by_intervals(data, intervals):
+    """
+    Splits a numpy ndarray into training and testing datasets based on provided intervals.
+
+    Parameters:
+        data (ndarray): The input ndarray to be split, with shape (n_samples, n_features).
+        intervals (list of tuples): A list of tuples representing intervals. Each tuple should
+            contain two integers representing the start and end indices of the interval.
+
+    Returns:
+        ndarray, ndarray: Two numpy ndarrays representing the training and testing datasets,
+            respectively.
+    """
+    test_data = []
+    for interval in test_intervals:
+        start, end = interval
+        test_data.extend(data[start:end])
+    test_data = np.array(test_data)
+    train_data = np.array([x for x in data if x not in test_data])
+    return train_data, test_data
+
+
+
+def convert_data(data_path = 'data/fit3d_train/train', out_dir = 'data/motion_3d', test_subjects = ['s03', 's011']):
     """
     Convert the dataset download into a dictionary and numpy arrays
 
     Returns:
     dictionary for train and test data
     """
+    root_dir = data_path
 
+    subjects = []
+    train_split_ids = []
+    test_split_ids = [] # index of test subjects data
+    camera_names = set()
+    camera_parameters = {}
+    camera_params = []
+
+    joints_3d = []
+    source = []
+
+    for root, dirnames, filenames in os.walk(root_dir, topdown=True): # Traverse data with dfs
+        basedir = os.path.basename(os.path.dirname(root)) # Current base directory
+
+        if not root: # Top of directory tree
+            subjects = dirnames 
+            # Check that test subjects are in dataset      
+            assert all(subject in all_subjects for subject in test_subjects), "Test subjects are not in dataset"
+        elif basedir in test_subjects:
+            test_split_ids.append(len(joints_3d))
+        elif basedir in subjects:
+            train_split_ids.append(len(joints_3d))
+        elif "joint_3d" in root:
+            for file in filenames:
+                with open(file, 'r') as joints:
+                    data = json.load(joints)
+                    joints_3d.append(data['joints']) # add joint data to array (17, 3)
+
+                    file_path = os.path.join(root, file)
+                    formatted_source = '_'.join(part.strip('./').replace('/', '_') for part in os.path.splitext(file_path)[0].split('/'))
+                    source.append(formatted_source * len(data['joints'])) # Add source label for current frame
+                    camera_params.append(camera_parameters[np.random.choice(list(camera_parameters.keys()))])
+        elif "camera_parameters" in root:
+            for dirnames in dirnames:
+                camera_names.add(basedir)
+        elif basedir in camera_names:
+            camera_name = basedir
+            for file in filenames:
+                with open(file, 'r') as param_data:
+                    data = json.load(param_data)
+                    intr_params = data.get('intrinsics_w_distortion', {})
+                    all_params = [param for sublist in intr_params.values() for param in sublist]
+                    if camera_name in camera_parameters and camera_parameters[camera_name] != all_params:
+                        raise AssertionError("Camera parameters do not match")
+                    camera_parameters[camera_name] = all_params
+
+    assert len(test_split_ids) == len(test_subjects), "Test subject split id missing"
+    assert len(train_split_ids) + len(test_split_ids) == len(subjects), "Train subject split id missing"
+    assert len(source) == len(joints_3d) == len(camera_params), "Joint, source and camera_params sizes are not equal"
+
+    N = len(joints_3d) # Number of totla frames in dataset
+
+    # Get interval of frames for test subjects
+    test_intervals = []
+    for i in test_split_ids:
+        test_intervals.append((i, min((x for x in train_split_ids if x > i), key=lambda x: abs(x-i))))
+
+    # Randomly sample 
+
+    # Info to all frames in dataset
+    joints_3d = np.array(shape=(N, 17, 3), dtype=np.float32)
+    joints_2d = project_to_2d(joints_3d, camera_params)
+    source = np.array(source)
+
+    joints_3d_train, joints_3d_test = split_data_by_intervals(joints_3d, test_intervals)
+    joints_2d_train, joints_2d_test = split_data_by_intervals(joints_2d, test_intervals)
+    soure_train, source_test = split_data_by_intervals(source, test_intervals)
+
+    
     return
