4f1f08097818ce9fe370552265d9364e55ef2e39
Andrew Boessen <boessena@bc.edu>
2024-05-05 14:02:12 -0400

    Fix imports

--- lib/model/drop_path.py
+++ lib/model/drop_path.py
@@ -1,6 +1,5 @@
 import torch
-import torch.nn
-import torch.nn.functional as F
+import torch.nn as nn
 
 def drop_path(x, drop_prob: float = 0., training: bool = False):
     if drop_prob == 0. or not training: # Dont dropout path while in inference mode
