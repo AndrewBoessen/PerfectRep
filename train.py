e2798dd8e1e25bdaff24b30bded3af1d241dc306
Andrew Boessen <boessena@bc.edu>
2024-05-19 18:03:09 -0500

    finish argparse

--- train.py
+++ train.py
@@ -18,7 +18,19 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog='PerfectRep Train',
+        description='A script for pretraining the 3D pose estimation model. This trains the model on the h3.6m and optional fit3d dataset'
+    )
     # Add Arguments for Data and Hyperparameters
-    parser.add_argument('-c', '--checkpoint', default=None, type=str, metavar='PATH', help='path to checkpoint binary to load (e.g. model.pt file)')
-    parser.add
+    parser.add_argument('-d', '--data_path', default='data/', type=str, metavar='PATH', help='path to training data directory')
+    parser.add_argument('-f', '--fit3d', default=False, type=bool, help='use fit3d data for training')
+    parser.add_argument('-c', '--checkpoint', default='', type=str, metavar='FILENAME', help='filename of checkpoint binary to load (e.g. model.pt file)')
+    parser.add_argument('-e', '--evaluate', default=False, type=bool, help='evaluate accuracy of model after each epoch')
+    parser.add_argument('-b', '--batch_size', default=1, type=int, help='batch size for training')
+    parser.add_argument('-e', '--epochs', default=10, type=int, help='number of epochs to train for')
+    
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
