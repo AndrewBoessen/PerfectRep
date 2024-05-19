0a2974557106a348e2b08e744a7a6bcb9c194130
Andrew Boessen <boessena@bc.edu>
2024-05-18 21:32:06 -0400

    loss functions

--- src/model/loss.py
+++ src/model/loss.py
@@ -49,3 +49,47 @@ def p_mpjpe(predicted, target):
     predicted_aligned = a*np.matmul(predicted, R) + t
     # Return MPJPE
     return np.mean(np.linalg.norm(predicted_aligned - target, axis=len(target.shape)-1), axis=1)
+
+# PyTorch-based errors (for losses)
+
+def loss_mpjpe(predicted, target):
+    """
+    Mean per-joint position error (i.e. mean Euclidean distance),
+    often referred to as "Protocol #1" in many papers.
+    """
+    assert predicted.shape == target.shape
+    return torch.mean(torch.norm(predicted - target, dim=len(target.shape)-1))
+    
+def weighted_mpjpe(predicted, target, w):
+    """
+    Weighted mean per-joint position error (i.e. mean Euclidean distance)
+    """
+    assert predicted.shape == target.shape
+    assert w.shape[0] == predicted.shape[0]
+    return torch.mean(w * torch.norm(predicted - target, dim=len(target.shape)-1))
+
+def loss_2d_weighted(predicted, target, conf):
+    assert predicted.shape == target.shape
+    predicted_2d = predicted[:,:,:,:2]
+    target_2d = target[:,:,:,:2]
+    diff = (predicted_2d - target_2d) * conf
+    return torch.mean(torch.norm(diff, dim=-1))
+    
+def n_mpjpe(predicted, target):
+    """
+    Normalized MPJPE (scale only), adapted from:
+    https://github.com/hrhodin/UnsupervisedGeometryAwareRepresentationLearning/blob/master/losses/poses.py
+    """
+    assert predicted.shape == target.shape
+    norm_predicted = torch.mean(torch.sum(predicted**2, dim=3, keepdim=True), dim=2, keepdim=True)
+    norm_target = torch.mean(torch.sum(target*predicted, dim=3, keepdim=True), dim=2, keepdim=True)
+    scale = norm_target / norm_predicted
+    return loss_mpjpe(scale * predicted, target)
+
+def weighted_bonelen_loss(predict_3d_length, gt_3d_length):
+    loss_length = 0.001 * torch.pow(predict_3d_length - gt_3d_length, 2).mean()
+    return loss_length
+
+def weighted_boneratio_loss(predict_3d_length, gt_3d_length):
+    loss_length = 0.1 * torch.pow((predict_3d_length - gt_3d_length)/gt_3d_length, 2).mean()
+    return loss_length
