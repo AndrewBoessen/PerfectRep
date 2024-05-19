717ce270c6a9cf0001636c98d602f00f859de5d4
Andrew Boessen <boessena@bc.edu>
2024-05-18 21:34:40 -0400

    limb velocity and angle loss

--- src/model/loss.py
+++ src/model/loss.py
@@ -93,3 +93,111 @@ def weighted_bonelen_loss(predict_3d_length, gt_3d_length):
 def weighted_boneratio_loss(predict_3d_length, gt_3d_length):
     loss_length = 0.1 * torch.pow((predict_3d_length - gt_3d_length)/gt_3d_length, 2).mean()
     return loss_length
+
+
+def get_limb_lens(x):
+    '''
+        Input: (N, T, 17, 3)
+        Output: (N, T, 16)
+    '''
+    limbs_id = [[0,1], [1,2], [2,3],
+         [0,4], [4,5], [5,6],
+         [0,7], [7,8], [8,9], [9,10],
+         [8,11], [11,12], [12,13],
+         [8,14], [14,15], [15,16]
+        ]
+    limbs = x[:,:,limbs_id,:]
+    limbs = limbs[:,:,:,0,:]-limbs[:,:,:,1,:]
+    limb_lens = torch.norm(limbs, dim=-1)
+    return limb_lens
+
+def loss_limb_var(x):
+    '''
+        Input: (N, T, 17, 3)
+    '''
+    if x.shape[1]<=1:
+        return torch.FloatTensor(1).fill_(0.)[0].to(x.device)
+    limb_lens = get_limb_lens(x)
+    limb_lens_var = torch.var(limb_lens, dim=1)
+    limb_loss_var = torch.mean(limb_lens_var)
+    return limb_loss_var
+
+def loss_limb_gt(x, gt):
+    '''
+        Input: (N, T, 17, 3), (N, T, 17, 3)
+    '''
+    limb_lens_x = get_limb_lens(x)
+    limb_lens_gt = get_limb_lens(gt) # (N, T, 16)
+    return nn.L1Loss()(limb_lens_x, limb_lens_gt)
+
+def loss_velocity(predicted, target):
+    """
+    Mean per-joint velocity error (i.e. mean Euclidean distance of the 1st derivative)
+    """
+    assert predicted.shape == target.shape
+    if predicted.shape[1]<=1:
+        return torch.FloatTensor(1).fill_(0.)[0].to(predicted.device)
+    velocity_predicted = predicted[:,1:] - predicted[:,:-1]
+    velocity_target = target[:,1:] - target[:,:-1]
+    return torch.mean(torch.norm(velocity_predicted - velocity_target, dim=-1))
+
+def loss_joint(predicted, target):
+    assert predicted.shape == target.shape
+    return nn.L1Loss()(predicted, target)
+
+def get_angles(x):
+    '''
+        Input: (N, T, 17, 3)
+        Output: (N, T, 16)
+    '''
+    limbs_id = [[0,1], [1,2], [2,3],
+         [0,4], [4,5], [5,6],
+         [0,7], [7,8], [8,9], [9,10],
+         [8,11], [11,12], [12,13],
+         [8,14], [14,15], [15,16]
+        ]
+    angle_id = [[ 0,  3],
+                [ 0,  6],
+                [ 3,  6],
+                [ 0,  1],
+                [ 1,  2],
+                [ 3,  4],
+                [ 4,  5],
+                [ 6,  7],
+                [ 7, 10],
+                [ 7, 13],
+                [ 8, 13],
+                [10, 13],
+                [ 7,  8],
+                [ 8,  9],
+                [10, 11],
+                [11, 12],
+                [13, 14],
+                [14, 15] ]
+    eps = 1e-7
+    limbs = x[:,:,limbs_id,:]
+    limbs = limbs[:,:,:,0,:]-limbs[:,:,:,1,:]
+    angles = limbs[:,:,angle_id,:]
+    angle_cos = F.cosine_similarity(angles[:,:,:,0,:], angles[:,:,:,1,:], dim=-1)
+    return torch.acos(angle_cos.clamp(-1+eps, 1-eps)) 
+
+def loss_angle(x, gt):
+    '''
+        Input: (N, T, 17, 3), (N, T, 17, 3)
+    '''
+    limb_angles_x = get_angles(x)
+    limb_angles_gt = get_angles(gt)
+    return nn.L1Loss()(limb_angles_x, limb_angles_gt)
+
+def loss_angle_velocity(x, gt):
+    """
+    Mean per-angle velocity error (i.e. mean Euclidean distance of the 1st derivative)
+    """
+    assert x.shape == gt.shape
+    if x.shape[1]<=1:
+        return torch.FloatTensor(1).fill_(0.)[0].to(x.device)
+    x_a = get_angles(x)
+    gt_a = get_angles(gt)
+    x_av = x_a[:,1:] - x_a[:,:-1]
+    gt_av = gt_a[:,1:] - gt_a[:,:-1]
+    return nn.L1Loss()(x_av, gt_av)
