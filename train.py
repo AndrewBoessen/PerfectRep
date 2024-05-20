a2aec9ff59e21e91b5df316a51aae800d629f4f2
Andrew Boessen <boessena@bc.edu>
2024-05-19 21:15:43 -0500

    add config file

--- train.py
+++ train.py
@@ -17,23 +17,15 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-SEED=0
-LR=0.0005
-CTXT_LEN=243 # 10 seconds of video at 24.3 fps
-FEAT_DIM=256
-MLP_RATIO=4
-DEPTH=4
-DIM_REP=512
-NUM_HEADS=8
-
 def parse_args():
     parser = argparse.ArgumentParser(
         prog='PerfectRep Train',
         description='A script for pretraining the 3D pose estimation model. This trains the model on the h3.6m and optional fit3d dataset'
     )
     # Add Arguments for Data and Hyperparameters
+    parser.add_argument('--config', default='train_config.yaml', type=str, metavar='FILENAME', help='config file')
     parser.add_argument('-d', '--data_path', default='data/', type=str, metavar='PATH', help='path to training data directory')
-    parser.add_argument('-f', '--fit3d', default=False, type=bool, help='use fit3d data for training')
+    parser.add_argument('-f', '--fit3d', default=False, action='store_true', type=bool, help='use fit3d data for training')
     parser.add_argument('-c', '--checkpoint', default='', type=str, metavar='FILENAME', help='filename of checkpoint binary to load (e.g. model.pt file)')
     parser.add_argument('-e', '--evaluate', default=False, type=bool, help='evaluate accuracy of model after each epoch')
     parser.add_argument('-b', '--batch_size', default=1, type=int, help='batch size for training')
@@ -65,3 +57,111 @@ def save_checkpoint(chk_path, epoch, lr, optimizer, model_pos, min_loss):
         'model_pos': model_pos.state_dict(),
         'min_loss' : min_loss
     }, chk_path)
+
+def evaluate(args, model_pos, test_loader, datareader):
+    '''
+    Copied from [MotionBERT: A Unified Perspective on Learning Human Motion Representations]
+                (https://github.com/Walter0807/MotionBERT/blob/main/train.py)
+    '''
+    print('INFO: Evaluating Model')
+    results_all = []
+    model_pos.eval()            
+    with torch.no_grad():
+        for batch_input, batch_gt in tqdm(test_loader):
+            N, T = batch_gt.shape[:2]
+            if torch.cuda.is_available():
+                batch_input = batch_input.cuda()
+            if args.no_conf:
+                batch_input = batch_input[:, :, :, :2]
+            if args.flip:    
+                batch_input_flip = flip_data(batch_input)
+                predicted_3d_pos_1 = model_pos(batch_input)
+                predicted_3d_pos_flip = model_pos(batch_input_flip)
+                predicted_3d_pos_2 = flip_data(predicted_3d_pos_flip)                   # Flip back
+                predicted_3d_pos = (predicted_3d_pos_1+predicted_3d_pos_2) / 2
+            else:
+                predicted_3d_pos = model_pos(batch_input)
+            if args.rootrel:
+                predicted_3d_pos[:,:,0,:] = 0     # [N,T,17,3]
+            else:
+                batch_gt[:,0,0,2] = 0
+
+            if args.gt_2d:
+                predicted_3d_pos[...,:2] = batch_input[...,:2]
+            results_all.append(predicted_3d_pos.cpu().numpy())
+    results_all = np.concatenate(results_all)
+    results_all = datareader.denormalize(results_all)
+    _, split_id_test = datareader.get_split_id()
+    actions = np.array(datareader.dt_dataset['test']['action'])
+    factors = np.array(datareader.dt_dataset['test']['2.5d_factor'])
+    gts = np.array(datareader.dt_dataset['test']['joints_2.5d_image'])
+    sources = np.array(datareader.dt_dataset['test']['source'])
+
+    num_test_frames = len(actions)
+    frames = np.array(range(num_test_frames))
+    action_clips = actions[split_id_test]
+    factor_clips = factors[split_id_test]
+    source_clips = sources[split_id_test]
+    frame_clips = frames[split_id_test]
+    gt_clips = gts[split_id_test]
+    assert len(results_all)==len(action_clips)
+    
+    e1_all = np.zeros(num_test_frames)
+    e2_all = np.zeros(num_test_frames)
+    oc = np.zeros(num_test_frames)
+    results = {}
+    results_procrustes = {}
+    action_names = sorted(set(datareader.dt_dataset['test']['action']))
+    for action in action_names:
+        results[action] = []
+        results_procrustes[action] = []
+    block_list = ['s_09_act_05_subact_02', 
+                  's_09_act_10_subact_02', 
+                  's_09_act_13_subact_01']
+    for idx in range(len(action_clips)):
+        source = source_clips[idx][0][:-6]
+        if source in block_list:
+            continue
+        frame_list = frame_clips[idx]
+        action = action_clips[idx][0]
+        factor = factor_clips[idx][:,None,None]
+        gt = gt_clips[idx]
+        pred = results_all[idx]
+        pred *= factor
+        
+        # Root-relative Errors
+        pred = pred - pred[:,0:1,:]
+        gt = gt - gt[:,0:1,:]
+        err1 = mpjpe(pred, gt)
+        err2 = p_mpjpe(pred, gt)
+        e1_all[frame_list] += err1
+        e2_all[frame_list] += err2
+        oc[frame_list] += 1
+    for idx in range(num_test_frames):
+        if e1_all[idx] > 0:
+            err1 = e1_all[idx] / oc[idx]
+            err2 = e2_all[idx] / oc[idx]
+            action = actions[idx]
+            results[action].append(err1)
+            results_procrustes[action].append(err2)
+    final_result = []
+    final_result_procrustes = []
+    summary_table = prettytable.PrettyTable()
+    summary_table.field_names = ['test_name'] + action_names
+    for action in action_names:
+        final_result.append(np.mean(results[action]))
+        final_result_procrustes.append(np.mean(results_procrustes[action]))
+    summary_table.add_row(['P1'] + final_result)
+    summary_table.add_row(['P2'] + final_result_procrustes)
+    print(summary_table)
+    e1 = np.mean(np.array(final_result))
+    e2 = np.mean(np.array(final_result_procrustes))
+    print('Protocol #1 Error (MPJPE):', e1, 'mm')
+    print('Protocol #2 Error (P-MPJPE):', e2, 'mm')
+    print('----------')
+    return e1, e2, results_all
+
+if __name__ == '__main__':
+    args = parse_args()
+    set_random_seed(args.seed)
+    cfg = get_config(args.config)
