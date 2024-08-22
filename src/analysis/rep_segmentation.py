5733ceba3395bcf62d9962503c5b985f5fc6488e
andrew <boessena@bc.edu>
2024-08-22 17:13:27 -0400

    uniform sample

--- src/analysis/rep_segmentation.py
+++ src/analysis/rep_segmentation.py
@@ -1,9 +1,10 @@
+import math
 import numpy as np
-from typing import Tuple
+from typing import Tuple, List
 from scipy.signal import argrelxtrema
 from src.utils.loss import mpjpe
 
-def init(p: np.array) -> Tuple(int, int):
+def init(p: np.ndarray) -> Tuple[int, int]:
     '''
     Initialize estimate of rep segments.
 
@@ -36,7 +37,7 @@ def init(p: np.array) -> Tuple(int, int):
 
     return best_tau, best_s
 
-def find_start(k_min: int, s: int, tau: int, p: np.array) -> int:
+def find_start(k_min: int, s: int, tau: int, p: np.ndarray) -> int:
     '''
     Find start frame of the series of rep. t_start
 
@@ -71,7 +72,7 @@ def find_start(k_min: int, s: int, tau: int, p: np.array) -> int:
     return best_t_start
 
 
-def auto_corr(s: int, tau: int, p: np.array) -> float:
+def auto_corr(s: int, tau: int, p: np.ndarray) -> float:
     '''
     Auto-correlation of rep segments.
 
@@ -84,7 +85,7 @@ def auto_corr(s: int, tau: int, p: np.array) -> float:
         tau: int
         Length of single repetition. Measuered in frames
 
-        p: np.array
+        p: np.ndarray
         Poses to split. 2d keypoints. (N, J, 2)
 
     Returns:
@@ -95,7 +96,7 @@ def auto_corr(s: int, tau: int, p: np.array) -> float:
     affinity = -mpjpe(p_strip[:-tau], p_strip[tau:]) # calculate affinity for each from in reps
     return np.mean(affinity) # average affinity over all valid frames
 
-def avg_aff(k_min: int, t_start: int, tau: int, p: np.array) -> float:
+def avg_aff(k_min: int, t_start: int, tau: int, p: np.ndarray) -> float:
     '''
     Compute average affinity of reps based on t start.
 
@@ -109,21 +110,21 @@ def avg_aff(k_min: int, t_start: int, tau: int, p: np.array) -> float:
         tau: int
         Number of frames in each rep
 
-        p: np.array
+        p: np.ndarray
         Poses. 2d keypoints (N, J, 2)
 
     Returns:
         float
         average affinity score
     '''
-    affinity = 0
+    affinity = 0.0
     for i in range(k_min):
         for j in range(k_min):
             affinity += seq_aff(t_start, i, j, tau, p) # affinity between reps i and j
 
     return affinity / k_min ** 2
 
-def seq_aff(t_start: int, i: int, j: int, tau: int, p: np.array) -> float:
+def seq_aff(t_start: int, i: int, j: int, tau: int, p: np.ndarray) -> float:
     '''
     Compute affinity between two repetitions.
 
@@ -142,7 +143,7 @@ def seq_aff(t_start: int, i: int, j: int, tau: int, p: np.array) -> float:
         tau: int
         Number of frames in a single rep
 
-        p: np.array
+        p: np.ndarray
         Poses. 2d keypoints (N, J, 2)
 
     Returns:
@@ -153,3 +154,13 @@ def seq_aff(t_start: int, i: int, j: int, tau: int, p: np.array) -> float:
     t_j = t_start + tau * j
 
     return np.mean(mpjpe(p[t_i: t_i+tau], p[t_j: t_j+tau]))
+
+def uniform_sample(T_i: List[int], n_s: int, p: np.ndarray):
+    result = []
+    step = (len(T_i) - 1) / (n_s - 1)
+    x = T_i[0]
+    while x <= T_i[-1]:
+        p_hat = p[math.floor(x)] * (1 - (x - math.floor(x))) + p[math.ceil(x)] * (x - math.floor(x))
+        result.append(p_hat)
+        x += step
+    return result
